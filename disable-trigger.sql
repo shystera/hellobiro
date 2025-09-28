b6a91fa23ff81c13f928bde5922290463751ccf0
shystera <shystera@example.com>
2025-09-28 12:39:50 +0530

    Remove fixed SQL files and clean up project structure

--- disable-trigger.sql
+++
@@ -1,8 +0,0 @@
--- Disable the problematic trigger completely
--- Run this in Supabase SQL Editor
-
--- Drop the trigger that's causing the 500 error
-DROP TRIGGER IF EXISTS on_auth_user_created ON auth.users;
-DROP FUNCTION IF EXISTS public.handle_new_user();
-
--- We'll handle profile creation manually in the client code instead
